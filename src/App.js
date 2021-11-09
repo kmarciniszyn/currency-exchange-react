6055f38ac43404793017f5c99cf478ffb1056e2f
Karolina Marciniszyn <marciniszyn.karolina@gmail.com>
2021-11-09 19:43:45 +0100

    moved "Form" into "Container"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Form from './Form';
+import Form from './Container/Form';
 import Container from './Container';
 import Clock from './Clock';
 import { currencies } from './currencies';
